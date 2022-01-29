f17017ab8b3dfe2771c7bf4541432104f9dc9c7c
saikinkatenai95 <saikinkatenai95@outlook.jp>
2022-01-29 21:07:56 +0900

    model単体テスト

--- spec/models/categorie_spec.rb
+++ spec/models/categorie_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Categorie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @categorie = FactoryBot.build(:categorie)
+  end
+
+  describe 'カテゴリー新規登録' do
+    it 'nameが空では登録できない' do
+      @categorie.name = ''
+      @categorie.valid?
+      expect(@categorie.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
