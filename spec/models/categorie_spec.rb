c4784875a23ee8f2e10794d2779aac9cc83d497f
saikinkatenai95 <saikinkatenai95@outlook.jp>
2022-01-29 21:36:44 +0900

    モデル単体　正常系テスト

--- spec/models/categorie_spec.rb
+++ spec/models/categorie_spec.rb
@@ -6,17 +6,24 @@ RSpec.describe Categorie, type: :model do
   end
 
   describe 'カテゴリー新規登録' do
-    it 'nameが空では登録できない' do
-      @categorie.name = ''
-      @categorie.valid?
-      expect(@categorie.errors.full_messages).to include("Name can't be blank")
+    context '新規登録できるとき'do
+      it 'nameが存在かつ重複していなければ登録できる' do
+        expect(@categorie).to be_valid
+      end
     end
-    it '重複したnameが存在する場合は登録できない' do
-      @categorie.save
-      another_name = FactoryBot.build(:categorie)
-      another_name.name = @categorie.name
-      another_name.valid?
-      expect(another_name.errors.full_messages).to include("Name has already been taken")
+    context '新規登録できないとき'do
+      it 'nameが空では登録できない' do
+        @categorie.name = ''
+        @categorie.valid?
+        expect(@categorie.errors.full_messages).to include("Name can't be blank")
+      end
+      it '重複したnameが存在する場合は登録できない' do
+        @categorie.save
+        another_name = FactoryBot.build(:categorie)
+        another_name.name = @categorie.name
+        another_name.valid?
+        expect(another_name.errors.full_messages).to include("Name has already been taken")
+      end
     end
   end
 end
