bc1875828eb917c2fa61efbd3723693c902d5c9a
saikinkatenai95 <saikinkatenai95@outlook.jp>
2022-01-30 19:26:53 +0900

    a

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,15 @@
 class IdeasController < ApplicationController
+  def index
+    json_response(@categorie.ideas)
+  end
+
+  def show
+    json_response()
+  end
+
+
+  private
+  
+  def idea_param
+    params.permit(:body).meage(category_id)
 end
